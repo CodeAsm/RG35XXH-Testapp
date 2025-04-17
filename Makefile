0f686a9ac615b00c55cd82c114ba2f312c60e407
Nico Vijlbrief <nico.vijlbrief@outlook.com>
2025-04-17 12:40:07 +0200

    Initial test

---
+++ Makefile
@@ -0,0 +1,46 @@
+#Set all your object files (the object files of all the .c files in your project, e.g. main.o my_sub_functions.o )
+OBJ = src/main.o src/render.o src/ini.o src/fx_cube.o  src/inprint2.o src/config.o 
+#Set any dependant header files so that if they are edited they cause a complete re-compile (e.g. main.h some_subfunctions.h some_definitions_file.h ), or leave blank
+DEPS = src/render.h src/input.h src/fx_cube.h  src/inline_font.h src/config.h
+
+#Any special libraries you are using in your project (e.g. -lbcm2835 -lrt `pkg-config --libs gtk+-3.0` ), or leave blank
+INCLUDES = $(shell pkg-config --libs sdl2 | sed 's/-mwindows//')
+
+#Set any compiler flags you want to use (e.g. -I/usr/include/somefolder `pkg-config --cflags gtk+-3.0` ), or leave blank
+local_CFLAGS = $(CFLAGS) $(shell pkg-config --cflags sdl2) -Wall -Wextra -O2 -pipe -I.
+
+#Set the compiler you are using ( gcc for C or g++ for C++ )
+CC = gcc
+
+#Set the filename extensiton of your C files (e.g. .c or .cpp )
+EXTENSION = .c
+
+SOURCE_DIR = src/
+
+#define a rule that applies to all files ending in the .o suffix, which says that the .o file depends upon the .c version of the file and all the .h files included in the DEPS macro.  Compile each object file
+%.o: %$(EXTENSION) $(DEPS)
+	$(CC) -c -o $@ $< $(local_CFLAGS)
+
+#Combine them into the output file
+#Set your desired exe output file name here
+tests: $(OBJ)
+	$(CC) -o $@ $^ $(local_CFLAGS) $(INCLUDES)
+
+libusb: INCLUDES = $(shell pkg-config --libs sdl2 libusb-1.0)
+libusb: local_CFLAGS = $(CFLAGS) $(shell pkg-config --cflags sdl2) -Wall -O2 -pipe -I. -DUSE_LIBUSB=1
+libusb: tests
+
+#Cleanup
+.PHONY: clean
+
+clean:
+	rm -f src/*.o *~ tests
+
+# PREFIX is environment variable, but if it is not set, then set default value
+ifeq ($(PREFIX),)
+    PREFIX := /usr/local
+endif
+
+install: tests
+	install -d $(DESTDIR)$(PREFIX)/bin/
+	install -m 755 tests $(DESTDIR)$(PREFIX)/bin/
---
+++ src/main.c
@@ -0,0 +1,120 @@
+// Copyright 2021 Jonne Kokkonen
+// Released under the MIT licence, https://opensource.org/licenses/MIT
+
+/* Uncomment this line to enable debug messages or call make with `make
+   CFLAGS=-DDEBUG_MSG` */
+// #define DEBUG_MSG
+
+#include <SDL.h>
+#include <signal.h>
+
+#include "SDL2_inprint.h"
+#include "config.h"
+#include "input.h"
+#include "render.h"
+
+enum state { QUIT, WAIT_FOR_DEVICE, RUN };
+
+enum state run = WAIT_FOR_DEVICE;
+uint8_t need_display_reset = 0;
+
+// Handles CTRL+C / SIGINT
+void intHandler() { run = QUIT; }
+
+//void close_serial_port() { disconnect(); }
+
+char input_text[256];
+
+int main(const int argc, char *argv[]) {
+
+
+  char *preferred_device = NULL;
+  if (argc == 3 && SDL_strcmp(argv[1], "--dev") == 0) {
+    preferred_device = argv[2];
+    SDL_Log("Using preferred device %s.\n", preferred_device);
+  }
+
+  // Initialize the config to defaults read in the params from the
+  // configfile if present
+  config_params_s conf = init_config();
+
+  // TODO: take cli parameter to override default configfile location
+  read_config(&conf);
+
+  uint8_t prev_input = 0;
+  uint8_t prev_note = 0;
+ // uint16_t zerobyte_packets = 0; // used to detect device disconnection
+
+  signal(SIGINT, intHandler);
+  signal(SIGTERM, intHandler);
+#ifdef SIGQUIT
+  signal(SIGQUIT, intHandler);
+#endif
+
+  // initialize all SDL systems
+  if (initialize_sdl(conf.init_fullscreen, conf.init_use_gpu) == -1)
+    run = QUIT;
+
+
+#ifdef DEBUG_MSG
+  SDL_LogSetAllPriority(SDL_LOG_PRIORITY_DEBUG);
+#endif
+
+  // main loop begin
+  do {
+      SDL_LogCritical(SDL_LOG_CATEGORY_ERROR, "Device not detected on begin loop.");
+      if (conf.wait_for_device == 1) {
+        run = WAIT_FOR_DEVICE;
+      } else {
+        run = QUIT;
+      }
+    
+    // wait until device is connected
+    if (conf.wait_for_device == 1) {
+      //static uint32_t ticks_poll_device = 0;
+      static uint32_t ticks_update_screen = 0;
+
+        screensaver_init();
+    
+
+      while (run == WAIT_FOR_DEVICE) {
+        // get current input
+        
+
+        if (SDL_GetTicks() - ticks_update_screen > 16) {
+          ticks_update_screen = SDL_GetTicks();
+          screensaver_draw();
+          render_screen();
+        }
+
+
+        SDL_Delay(conf.idle_ms);
+      }
+    } else {
+      // classic startup behaviour, exit if device is not found
+      
+     
+        close_renderer();
+        kill_inline_font();
+        SDL_Quit();
+        return -1;
+      }
+    
+
+    // main loop
+    while (run == RUN) {
+        //TODO
+      handle_input();
+      render_screen();
+      SDL_Delay(conf.idle_ms);
+    }
+  } while (run > QUIT);
+  // main loop end
+
+  // exit, clean up
+  SDL_Log("Shutting down\n");\
+  close_renderer();
+//  close_serial_port();
+  SDL_Quit();
+  return 0;
+}
